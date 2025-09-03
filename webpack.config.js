7572cb224f00d6528babdbf6f1cf1619033a04fe
dianannperla <waafirmelyza@hotmail.com>
2025-09-03 18:10:58 +0100

    Update output filename for better clarity.

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'bundle.js',
+    filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
   },
   module: {
